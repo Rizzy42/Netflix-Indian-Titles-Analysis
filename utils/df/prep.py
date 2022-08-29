a48c2626a83a153dab20570f9e92f0f72743a684
shaurCode <shaurya.rishi@yahoo.com>
2022-08-29 20:40:43 +0400

    Removed asterisks

--- utils/df/prep.py
+++ utils/df/prep.py
@@ -1,6 +1,6 @@
-def checkForNullEntries(*in_df):
+def checkForNullEntries(in_df):
 	return in_df.isnull().any()
 
-def filterIncompleteEntries(*in_df, col):
+def filterIncompleteEntries(in_df, col):
 	entries = in_df[in_df[col].isnull()]
 	return entries
